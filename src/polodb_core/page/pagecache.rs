a4d2537802799c87f44c8ee42c93bf024e71aa69
Vincent Chan <okcdz@diverse.space>
2020-10-13 10:00:04 +0800

    feat: change license to MIT

--- src/polodb_core/page/pagecache.rs
+++ src/polodb_core/page/pagecache.rs
@@ -1,18 +1,3 @@
-/*
- * Copyright (c) 2020 Vincent Chan
- *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free Software
- * Foundation; either version 3, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Lesser General Public License along with
- * this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 use libc::{ malloc, free };
 use std::collections::HashMap;
 use std::ptr::null_mut;
