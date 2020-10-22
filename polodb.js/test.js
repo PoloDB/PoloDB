541895e02f597c5b2fb833967f5d4504bf29a46e
Vincent Chan <okcdz@diverse.space>
2020-10-22 23:17:46 +0800

    Revert "feat: add CollectionObject"
    This reverts commit 8329420d44f948441dc7338ad6b444c8c302956f.

--- polodb.js/test.js
+++ polodb.js/test.js
@@ -1,6 +1,5 @@
-const { Database, DbArray, Document, Value, version, UTCDatetime } = require("./index");
+const { Database, DbArray, Document, Value, version } = require("./index");
 
-const time = UTCDatetime.fromTimestamp(1000);
 console.log('version', version());
 
 const db = new Database("/tmp/test-node-3");
@@ -17,52 +16,24 @@ console.log(oid.hex());
 try {
   const collection = db.collection("test_col");
 
-  // for (let i = 0; i < 10; i++) {
+  // for (let i = 0; i < 100; i++) {
   //   collection.insert({
   //     hello: i,
   //   });
   // }
 
-  // const count = collection.update({
-  //   hello: 600, 
-  // }, {
-  //   $min: {
-  //     hello: 400,
-  //   }
-  // });
+  const count = collection.update({
+    hello: 600, 
+  }, {
+    $min: {
+      hello: 400,
+    }
+  });
 
-  // console.log('count:', count);
-
-  // db.createCollection('test_col2');
-  // const collection = db.collection("test_col");
-
-  // for (let i = 0; i < 10; i++) {
-  //   collection.insert({
-  //     _id: i,
-  //     content: i.toString(),
-  //   });
-  // }
+  console.log('count:', count);
 
   const allData = collection.findAll();
-
-  const first = allData[2];
-
-  allData.forEach((item, index) => {
-    console.log('id: ', index, ', ', item._id.toString());
-    console.log(item);
-  });
-
-  const firstId = first['_id'];
-
-  console.log('firstId', firstId);
-  console.log('first hex: ', firstId.toString());
-  const result = collection.find({
-    _id: firstId,
-    // hello: 3
-  });
-
-  console.log(result);
-  // console.log(result[0].toString());
+  console.log(allData);
 } catch(e) {
   console.error(e);
 } finally {
