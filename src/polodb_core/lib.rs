93f3e1cd4cf315f8aa7d40e7b0ffd8e18751b354
Vincent Chan <okcdz@diverse.space>
2020-12-08 23:17:40 +0800

    feat: update READEME.md

--- src/polodb_core/lib.rs
+++ src/polodb_core/lib.rs
@@ -11,8 +11,8 @@
 //! # Installation
 //! ```toml
 //! [dependencies]
-//! polodb_core = "0.4.3"
-//! polodb_bson = "0.4.3"
+//! polodb_core = "0.5.2"
+//! polodb_bson = "0.5.2"
 //! ```
 //!
 //! # Usage
