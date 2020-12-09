8675d8e55184f03f8106f15fb1817d57cd1db8be
Vincent Chan <okcdz@diverse.space>
2020-12-09 22:19:03 +0800

    release(0.6.0): npm and pypi

--- polodb.js/scripts/download-lib.js
+++ polodb.js/scripts/download-lib.js
@@ -5,7 +5,7 @@ const path = require('path');
 const os = require('os');
 const crypto = require('crypto');
 
-const version = '0.5.1';
+const version = '0.6.0';
 
 const platform = os.platform();
 const arch = os.arch();
