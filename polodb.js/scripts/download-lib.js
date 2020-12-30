131e1f099fc269a89a54421ffedb559b348f97c3
Vincent Chan <okcdz@diverse.space>
2020-12-31 00:03:37 +0800

    release(js): 0.8.0

--- polodb.js/scripts/download-lib.js
+++ polodb.js/scripts/download-lib.js
@@ -5,7 +5,7 @@ const path = require('path');
 const os = require('os');
 const crypto = require('crypto');
 
-const version = '0.6.0';
+const version = '0.8.0';
 
 const platform = os.platform();
 const arch = os.arch();
