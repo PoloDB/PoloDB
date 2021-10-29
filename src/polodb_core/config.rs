1fd0f537580bc102bb0a24ebbe7c7ade5704664b
Vincent Chan <okcdz@diverse.space>
2021-10-29 14:25:43 +0800

    feat: upgrade version

--- src/polodb_core/config.rs
+++ src/polodb_core/config.rs
@@ -4,6 +4,7 @@ use std::num::NonZeroU64;
 pub struct Config {
     pub init_block_count:  NonZeroU64,
     pub journal_full_size: u64,
+    pub check_db_version:  bool,
 }
 
 impl Default for Config {
@@ -12,6 +13,7 @@ impl Default for Config {
         Config {
             init_block_count:  NonZeroU64::new(16).unwrap(),
             journal_full_size: 1000,
+            check_db_version: true,
         }
     }
 
