0d8dbe200d590338d5c298b5821b41057e06454f
Vincent Chen <okcdz@diverse.space>
2023-01-30 13:18:17 +0800

    fix(ci): transaction

--- src/polodb_core/transaction.rs
+++ src/polodb_core/transaction.rs
@@ -1,3 +1,4 @@
+use std::cell::Cell;
 use serde::{Serialize, Deserialize};
 
 #[derive(Eq, PartialEq, Copy, Clone, Serialize, Deserialize)]
@@ -7,10 +8,43 @@ pub enum TransactionType {
     Write = 2,
 }
 
-#[derive(Eq, PartialEq, Copy, Clone)]
+#[derive(Eq, PartialEq, Clone)]
 pub(crate) enum TransactionState {
     NoTrans,
     User,
     UserAuto,
-    DbAuto,
+    DbAuto(Cell<i32>),
+}
+
+impl TransactionState {
+
+    pub(crate) fn new_db_auto() -> TransactionState {
+        TransactionState::DbAuto(Cell::new(1))
+    }
+
+    #[inline]
+    pub(crate) fn is_no_trans(&self) -> bool {
+        if let TransactionState::NoTrans = self {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub(crate) fn acquire(&self) {
+        if let TransactionState::DbAuto(counter) = self {
+            counter.set(counter.get() + 1)
+        }
+    }
+
+    pub (crate) fn release(&self) -> bool {
+        if let TransactionState::DbAuto(counter) = self {
+            counter.set(counter.get() - 1);
+            let value = counter.get();
+            value == 0
+        } else {
+            false
+        }
+    }
+
 }
