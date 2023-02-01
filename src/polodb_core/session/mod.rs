27cb07c14aa6b732f7598585be1a4084a63ea187
Vincent Chen <okcdz@diverse.space>
2023-02-01 20:23:53 +0800

    feat: add dynamic session

--- src/polodb_core/session/mod.rs
+++ src/polodb_core/session/mod.rs
@@ -3,7 +3,9 @@ mod session;
 mod pagecache;
 mod base_session;
 mod data_page_allocator;
+mod dynamic_session;
 
 pub use client_session::ClientSession;
 pub(crate) use session::Session;
 pub(crate) use base_session::BaseSession;
+pub(crate) use dynamic_session::DynamicSession;
