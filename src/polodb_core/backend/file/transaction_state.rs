ea8c13ae2eca0b96aadc0cfcc28e94948204a3be
Vincent Chen <okcdz@diverse.space>
2023-02-07 16:53:04 +0800

    feat: use transactional map in journal manager

--- src/polodb_core/backend/file/transaction_state.rs
+++ src/polodb_core/backend/file/transaction_state.rs
@@ -1,22 +1,28 @@
 use std::collections::BTreeMap;
+use crate::data_structures::trans_map::{TransMap, TransMapDraft};
 use crate::transaction::TransactionType;
 
 pub(super) struct TransactionState {
     pub(super) ty: TransactionType,
-    pub(super) offset_map: BTreeMap<u32, u64>,
+    pub(super) offset_map: TransMapDraft<u32, u64>,
     pub(super) frame_count: u32,
     pub(super) db_file_size: u64,
 }
 
 impl TransactionState {
 
-    pub(super) fn new(ty: TransactionType, frame_count: u32, db_file_size: u64) -> TransactionState {
+    pub(super) fn new(ty: TransactionType, offset_map: TransMap<u32, u64>, frame_count: u32, db_file_size: u64) -> TransactionState {
         TransactionState {
             ty,
-            offset_map: BTreeMap::new(),
+            offset_map: TransMapDraft::new(offset_map),
             frame_count,
             db_file_size,
         }
+    }
+
+    #[inline]
+    pub fn set_type(&mut self, ty: TransactionType) {
+        self.ty = ty;
     }
 
 }
