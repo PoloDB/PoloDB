7b79dc6ff804bf76750d25d6887c108b82388f33
Danilo Guanabara <danguafer@gmail.com>
2024-09-05 23:50:10 -0300

    Getting rid of warnings (#170)

--- src/polodb_core/utils/file_lock.rs
+++ src/polodb_core/utils/file_lock.rs
@@ -12,85 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[cfg(not(target_os = "windows"))]
 use std::fs::File;
+
+#[cfg(not(target_os = "windows"))]
 use crate::{Error, Result};
-
-#[cfg(target_os = "windows")]
-use std::os::windows::io::AsRawHandle;
-
-#[cfg(target_os = "windows")]
-pub(crate) fn exclusive_lock_file(file: &File) -> Result<()> {
-    use winapi::um::fileapi::LockFileEx;
-    use winapi::um::minwinbase::OVERLAPPED;
-    use winapi::um::minwinbase::{LOCKFILE_EXCLUSIVE_LOCK, LOCKFILE_FAIL_IMMEDIATELY};
-    use winapi::ctypes;
-
-    let handle = file.as_raw_handle();
-
-    let bl = unsafe {
-        let overlapped: *mut OVERLAPPED = libc::malloc(std::mem::size_of::<OVERLAPPED>()).cast::<OVERLAPPED>();
-        libc::memset(overlapped.cast::<libc::c_void>(), 0, std::mem::size_of::<OVERLAPPED>());
-        let result: i32 = LockFileEx(
-            handle.cast::<ctypes::c_void>(),
-            LOCKFILE_EXCLUSIVE_LOCK | LOCKFILE_FAIL_IMMEDIATELY,
-            0, 0, 0, overlapped);
-        libc::free(overlapped.cast::<libc::c_void>());
-        result
-    };
-
-    if bl == 0 {
-        return Err(Error::Busy);
-    }
-
-    Ok(())
-}
-
-#[cfg(target_os = "windows")]
-pub(crate) fn shared_lock_file(file: &File) -> Result<()> {
-    use winapi::um::fileapi::LockFileEx;
-    use winapi::um::minwinbase::OVERLAPPED;
-    use winapi::um::minwinbase::LOCKFILE_FAIL_IMMEDIATELY;
-    use winapi::ctypes;
-
-    let handle = file.as_raw_handle();
-
-    let bl = unsafe {
-        let overlapped: *mut OVERLAPPED = libc::malloc(std::mem::size_of::<OVERLAPPED>()).cast::<OVERLAPPED>();
-        libc::memset(overlapped.cast::<libc::c_void>(), 0, std::mem::size_of::<OVERLAPPED>());
-        let result: i32 = LockFileEx(handle.cast::<ctypes::c_void>(), LOCKFILE_FAIL_IMMEDIATELY, 0, 0, 0, overlapped);
-        libc::free(overlapped.cast::<libc::c_void>());
-        result
-    };
-
-    if bl == 0 {
-        return Err(Error::Busy);
-    }
-
-    Ok(())
-}
-
-#[cfg(target_os = "windows")]
-pub(crate) fn unlock_file(file: &File) -> Result<()> {
-    use winapi::um::fileapi::UnlockFileEx;
-    use winapi::um::minwinbase::OVERLAPPED;
-    use winapi::ctypes;
-
-    let handle = file.as_raw_handle();
-
-    let bl = unsafe {
-        let overlapped: *mut OVERLAPPED = libc::malloc(std::mem::size_of::<OVERLAPPED>()).cast::<OVERLAPPED>();
-        libc::memset(overlapped.cast::<libc::c_void>(), 0, std::mem::size_of::<OVERLAPPED>());
-        let result: i32 = UnlockFileEx(handle.cast::<ctypes::c_void>(), 0, 0, 0, overlapped);
-        libc::free(overlapped.cast::<libc::c_void>());
-        result
-    };
-
-    if bl == 0 {
-        return Err(Error::Busy);
-    }
-
-    Ok(())
-}
 
 #[cfg(not(target_os = "windows"))]
 #[allow(dead_code)]
