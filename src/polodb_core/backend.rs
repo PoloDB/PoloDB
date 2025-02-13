f78eddb2b5b4e820c303406c614525723631e694
SapryWenInera <97859194+SapryWenInera@users.noreply.github.com>
2025-02-13 09:43:39 -0300

    Restrict Backend for types of known sizes

--- src/polodb_core/backend.rs
+++ src/polodb_core/backend.rs
@@ -1,7 +1,10 @@
 use crate::{db::Result, Config};
 use std::path::Path;
 
-pub trait Backend {
+pub trait Backend
+where
+    Self: Sized,
+{
     fn try_open(path: &Path) -> Result<Self>;
     fn try_open_with_config(path: &Path, config: Config) -> Result<Self>;
 }
